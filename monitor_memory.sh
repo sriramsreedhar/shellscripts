3f28c5ab74613ec8d3577f4763e54b518314cab9
Sriram Sreedhar <sriram003@gmail.com>
2013-12-17 10:41:06 -0600

    Multiple commands for Memory Management to one date output

--- monitor_memory.sh
+++ monitor_memory.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-sudo top -c -m -b  -n 1  | head -n 25 > /var/log/memory/top.out.$(date +"%m-%d-%Y::%H::%M")
+{ echo -e "=====Users Logged in =====\n"; w; echo -e " =========Top Result=======\n"; sudo top -c -m -b  -n 1  | head -n 25;} > /var/log/memory/top.out.$(date +"%m-%d-%Y::%H::%M")
