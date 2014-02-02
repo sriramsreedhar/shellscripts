b772765f240cc5976bb60d0a34ee696f5fa7db45
Sriram Sreedhar <sriram003@gmail.com>
2014-02-01 22:45:08 -0600

    Simple Python scripts

---
+++ for_python.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+names = ["sriram", "Harini", "Rakesh", "Mahesh", "Janaki Patti", "Sreeja"]
+
+for i in names:
+	print(i)
---
+++ if_python.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+if 40 > 30:
+	print("This is true")
