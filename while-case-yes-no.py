5cd1063851727359dc26ba16eeb165962cf6d6ff
sriramsreedhar <RSD\ssreedha@RDRE1-TEECFMX05.rsd.crumprsd.com>
2019-11-08 15:56:39 -0500

    while statement used for yes/no option

---
+++ while-case-yes-no.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true; do
+    read -p "Do you wish to install this program?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
